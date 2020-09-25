4eb665dd8bff60aac77e826426cd7427b7b3494b
Jose Salazar Mendez <thatsenpai@Joses-MBP.eau.wi.charter.com>
2020-09-25 16:31:21 -0500

    Functions
    Basic functions

---
+++ functions.py
@@ -0,0 +1,36 @@
+def area(width, height):
+    result = width * height
+    return result
+
+result = area(5, 6)
+print(result)
+
+result = area(6, 7)
+print(result)
+
+result = area(8, 9)
+print(result)
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+result = subtract(6, 7)
+print(result)
+
+result = subtract(8, 9)
+print(result)
+
+def divide(number1, number2):
+    result = number1 / number2
+    return result
+
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+result = add(6, 7)
+print(result)
+
+result = add(8, 9)
+print(result)
